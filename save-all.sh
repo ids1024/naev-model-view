3bf838ff5748b991d7087e8eb6b35f002c38d2f2
Ian Douglas Scott <ian@iandouglasscott.com>
2020-03-21 11:51:36 -0700

    Skip viper*.obj

--- save-all.sh
+++ save-all.sh
@@ -3,5 +3,8 @@
 rm -rf png-exports
 mkdir png-exports
 for i in 3d/*/*.obj; do
+	if echo $i | grep -q 'viper.*.obj'; then
+		continue
+	fi
 	./obj-view.py $i --res 2048 --rot 90 --save "png-exports/$(basename $i .obj).png"
 done
