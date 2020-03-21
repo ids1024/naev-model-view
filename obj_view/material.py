9c22fb075dc5c8d86fbdb48e41a31ed183a63ac7
Ian Douglas Scott <ian@iandouglasscott.com>
2020-03-21 15:25:40 -0700

    Modularize code

---
+++ obj_view/material.py
@@ -0,0 +1,14 @@
+from .texture import solidTexture
+
+class Material:
+    Ka = None
+    Kd = None
+    Ks = None
+    Ns = None
+    Ni = None
+    bm = 0.0
+    d = 1.0
+
+    def __init__(self):
+       self.map_Kd = solidTexture(1, 1, 1)
+       self.map_Bump = solidTexture(0, 0, 0)
---
+++ obj_view/object.py
@@ -0,0 +1,5 @@
+class Object:
+    radius = 0.0
+    def __init__(self):
+        self.vertices = []
+        self.mtl_list = []
